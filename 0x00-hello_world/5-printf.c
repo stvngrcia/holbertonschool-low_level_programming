e3d44ddb6a02658a46b1374c4a42204c172a9722
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-07-31 21:31:58 -0700

    THis is 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+include <stdio.h> /*library command printf is stored in*/
+
+int main()
+{
+  printf ("with proper grammer, but the outcome is a piece of art,  \n");
+
+  return 0;  /*if failed returns 0*/
+}
+
+/*curly braces contain argument*/
+/* " " are used to contain statement*/
+/* ; are used to end statement like period*/
