c6948d15dfbb9304311d01f78f36e50e4f77ea95
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-07-31 21:29:29 -0700

    This is 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
+#include <stdio.h> /*library command puts is stored in */
 
-#include <stdio.h>
-
-int main ()
+int main()
 {
-	puts ("Programming is like building a multilingual puzzle");
 
-return 0;
+  puts ("Programming is like building a multilingual puzzle");
+
+  return 0; /* if code fails returns 0 */
 }
+/*curly braces contains arguements */
+/* " " are used to contain statement */
+/* ; are used to end statement */
