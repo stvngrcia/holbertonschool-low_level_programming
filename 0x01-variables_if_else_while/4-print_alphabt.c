b9020e7ada40695b80855b1f51cf3b5a419d6ffa
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-08-13 16:16:17 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    
+    char a;
+    char b;
+
+    a = 'a';
+    b = '\n';
+    
+    while (a <= 'z')
+    {
+        putchar(a);
+
+        if (a == 'p' || a == 'd')
+        {
+            a = a + 2;
+        } 
+        else 
+        {
+            a = a + 1;
+        }
+    }
+
+    putchar(b);
+
+    return 0;
+}
