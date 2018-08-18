79aa0c31afb2885219cd75e84b44e37429c2d7c8
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-08-18 16:45:30 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,24 @@
 #include <time.h>
 #include <stdio.h>
 
+
 int main(void)
 {
-  int n;
+	int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-
-  if (n > '0')
-  {
-    printf("%d is positive\n", n);
-  }else if(n == '0')
-  {
-    printf("%d is zero\n", n);
-  }else{
-    printf("%d is negative\n", n);
-  }
-  return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > '0')
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
