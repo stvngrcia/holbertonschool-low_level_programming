16b6f1c88c6062a5e222d5627f0222cfa9c2b8eb
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-08-18 12:37:32 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,24 +7,21 @@
 /*have no idea what I'm doing for this part*/
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  
-  if (n > 0)
-    { 
-      printf("%d is positive\n", n);
-    }
-  else
-    { if (n == 0)
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{ printf("%d is positive\n", n); }
+else
+if (n == 0)
+{
 	{ printf("%d is zero\n", n);
 	}
-      else
+else
 	{ printf("%d is negative\n", n);
-	}
-    }  
-  return (0);
+}}
+return (0);
 }
 
-/* %d is representing a part of memory for integer (in this case n) to fill in value when printing */
+
