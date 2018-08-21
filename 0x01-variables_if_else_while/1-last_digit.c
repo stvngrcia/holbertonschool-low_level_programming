7df8b5425980c8400b46cef95c4b2d6f0d811ed3
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-08-21 16:05:58 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,34 +9,47 @@
  */
 int main(void)
 {
-	int n;
+	char n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	printf("Last digit of %d is ", n);
-
-	while (n > 9)
+	if (n % 10 > 5)
 	{
-		n = n - 10;
-		}
-		while (n < 0)
+		printf("%d and is greater than 5\n", n % 10);
+	}
+	else
+	{
+		if (n % 10 < 6 && n % 10 > 0)
 		{
-			n = n + 10;
-			}
-			if (n > 5)
+			printf("%d and is less than 6 and not 0\n", n % 10);
+		}
+		else
+		{
+			if (n % 10 == 0)
 			{
-				printf("%d and is greater than 5\n", n);
+				printf("%d and is 0\n", n % 10);
+			}
+			else
+			{
+				if ((n * -1) % 10 > 5)
+				{
+					printf("%d and is greater than 5\n", (n * -1) % 10);
 				}
 				else
 				{
-					if (n < 6 && n != 0)
-					{ printf("%d and is less than 6 and not 0\n", n);
+					if ((n * -1) % 10 < 6 && (n * -1) % 10 != 0)
+					{
+						printf("%d and is less than 6 and not 0\n", (n * -1) % 10);
 					}
 					else
 					{
-						printf("%d and is 0\n", n);
-						}
-						}
-						return (0);
-						}
+						printf("%d and is 0\n", (n * -1) % 10);
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
