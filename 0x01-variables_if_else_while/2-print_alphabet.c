d04edd52180670c34c68a112e8c1e88a98a38edd
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-08-21 16:13:04 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - prints alphabet a-z
+ * Return: 0
+ */
+
 int main(void)
 {
-    char a;  /* declaration of character */
-    char b;
+char a;  /* declaration of character */
+char b;
 
-    a = 'a';
-    b = '\n';
+a = 'a';
+b = '\n';
 
-
-    while (a <= 'z')
-    { 
-    putchar(a); /* putchar allows you to not have to store value and recall */
-    a = a + 1;
-    }
-
-    putchar(b);
-
-    return 0;
+while (a <= 'z')
+{
+	putchar(a); /* putchar allows you to not have to store value and recall */
+	a = a + 1;
+}
+putchar(b);
+return (0);
 }
