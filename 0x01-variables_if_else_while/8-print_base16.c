af9091c81bd17a872e90eb094f08e9676b7cd564
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-08-28 17:16:35 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 
+/**
+ * main - prints 0-9 then a-f
+ * Return: 0
+ */
+
 int main(void)
 {
-    char a;
-    char l;
-    char n;
+char a;
+char l;
+char n;
 
-    a = '0';
-    l = 'a';
-    n = '\n';
+a = '0';
+l = 'a';
+n = '\n';
 
-    while (a <= '9')
-    {
-        putchar(a);
-        a = a + 1;
-    }
-    while (l <= 'f')
-    {
-        putchar(l);
-        l = l + 1;
-    }
-    putchar(n);
-    return 0;
+while (a <= '9')
+{
+	putchar(a);
+	a = a + 1;
+}
+
+while (l <= 'f')
+{
+	putchar(l);
+	l = l + 1;
+}
+
+putchar(n);
+return (0);
 }
