270942caaad5f66206bf96e4405995e5d5dfe82f
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-08-28 17:09:13 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - prints 0-9 with newline only using putchar
+ * Return: 0
+ */
+
+int main(void)
 {
-    int a;
-    int n;
+int a;
+int n;
 
-    a = '0';
-    n = '\n';
+a = '0';
+n = '\n';
 
-    while (a <= '9')
-    {
-        putchar(a);
-        a = a + 1;
-    }
+while (a <= '9')
+{
+	putchar(a);
+	a = a + 1;
+}
 
-    putchar(n);
-    return 0;
+putchar(n);
+
+return (0);
 }
