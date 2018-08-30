d3ba41858600a67eb7034b1fd1983e8f5b639aa2
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-08-29 20:55:14 -0700

    10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 
+/**
+ * main - prints 00-99 followed by comma and space except for 99
+ * Return: 0
+ */
+
 int main(void)
 {
-    int a;
-    int b;
-    int c;
-    int s;
-    int n;
+int a;
+int b;
+int c;
+int s;
+int n;
 
-    a = '0'; 
-    b = '0';
-    c = ',';
-    s = ' ';
-    n = '\n';
+a = '0';
+b = '0';
+c = ',';
+s = ' ';
+n = '\n';
 
-    while(a <= '9')
-    {
-        putchar(a);
-        putchar(b);
-        b = b + 1;
+while (a <= '9')
+{
+	putchar(a);
+	putchar(b);
+	b = b + 1;
 
-    if(b == ':')
-    {
-        b = '0';
-        a = a + 1;
-    } 
-    if(a != ':' && b != ':')
-    {
-        putchar(c);
-        putchar(s);
-    }
-    }
-    putchar(n);
-    return 0;
+	if (b == ':')
+	{
+		b = '0';
+		a = a + 1;
+	}
+	if (a != ':' && b != ':')
+	{
+		putchar(c);
+		putchar(s);
+	}
+}
+
+putchar(n);
+return (0);
 }
