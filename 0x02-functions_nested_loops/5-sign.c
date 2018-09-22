9db57d82ad00796d17966e043652dba57fb8e8ae
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-09-22 16:33:58 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * print_sign - 
- * Return: 
- * @n: 
+ * print_sign - prints the symbol and assigns return value
+ * Return: 1 if > 0, 0 if 0, or -1 if < 0
+ * @n: assigns return value
  **/
 
 int print_sign(int n)
