ed9f45cd452c1b3511d75e4ede2b1e9444717639
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-09-24 21:40:27 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,19 @@
 
 /**
  * print_last_digit - calculates the last digit of number provided in 7-main.c
- * Return: returns calculated last digit of number
  * @r: holds the value of the last digit
+ * Return: returns calculated last digit of number
  **/
 
 int print_last_digit(int r)
 {
+	
+	r = r % 10;
 	if (r < 0)
 	{
 		r = r * -1;
 	}
-	r = r % 10;
-	_putchar(48 + r);
+	_putchar('0' + r);
+
 	return (r);
 }
