538396b2ab0b99c8d8907a87d55a3ad255c23f29
stvngrcia <Revel@Revels-MacBook-Pro.local>
2018-10-06 12:06:16 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - calculates the total of two integers provided in 10-main
+ * Return: Returns sum of two integers to 10-main
+ * @a: holds value of first integer
+ * @b: holds value of second integer
+ **/
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
